c71864f0de9e85f7eea3453ef3c7006e42a1dd0b
Anna Tran <annatran@Annas-MacBook-Air.local>
2019-12-29 22:31:16 +0000

    image carousel

---
+++ app.js
@@ -0,0 +1,6 @@
+const carouselSlide = document.querySelector('.image-slideshow');
+const carouselImages = document.querySelectorAll('.image-slideshow img');
+const previousButton = document.getElementById('#prevButton');
+const nextButton = document.getElementById('#nextbutton');
+
+let count = 1;
