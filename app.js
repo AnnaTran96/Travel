b2f4337ee3f6958dd51a3748036fc6f59f13c98d
Anna Tran <annatran@Annas-MacBook-Air.local>
2019-12-30 17:40:42 +0000

    background

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
-const carouselSlide = document.querySelector('.image-slideshow');
-const carouselImages = document.querySelectorAll('.image-slideshow img');
-const previousButton = document.getElementById('#prevButton');
-const nextButton = document.getElementById('#nextbutton');
+var slides = document.querySelector('.slider-items').children;
+var rightBtn = document.querySelector('.rightButton');
+var leftBtn = document.querySelector('.leftButton');
+var index = 0;
 
-let count = 1;
+rightBtn.onclick=function() {
+    // console.log("click");
+    next("next");
+}
+
+leftBtn.onclick=function() {
+    // console.log("click");
+    next("left");
+}
+
+function next(direction) {
+    if(direction == "next"){
+        index++;
+        if(index == slides.length){
+            index = 0;
+        }
+    } else {
+        if (index == 0){
+            index = slides.length - 1;
+        } else {
+            index--;
+        }
+    }
+    for (i=0; i < slides.length; i++){
+        slides[i].classList.remove("active");
+    }
+    slides[index].classList.add("active");
+}
